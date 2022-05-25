3d5d918c04c3ea2288d12baee818af9d0a735413
LUCKY TIWARI <89370329+Loki1920@users.noreply.github.com>
2022-05-25 21:57:52 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,31 @@
+#QUICK SORT ALGORITHM
+#function to find correct position of pivot
+
+def pivot_place(list1,first,last):
+  pivot = list1[first]
+  left = first + 1
+  right = last 
+  
+  while True:
+    while left <= right and list1[left] >= pivot:
+      left += 1
+    while left <= right and list1[right] <= pivot:
+      right = right -1 
+    if right < left:
+      break
+    else:
+      list1[left],list1[right] = list1[right],list1[left]
+  list1[first],list1[right] = list1[right],list1[first]
+  return right 
+
+def Quicksort(list1,first,last):
+  if first < last :
+    p = pivot_place(list1,first,last)
+    Quicksort(list1,first,p-1)
+    Quicksort(list1,p+1,last)
+
+list1 = [56,26,93,17,3,44]
+n = len(list1)
+Quicksort(list1,0,n-1)
+print(list1)
+
